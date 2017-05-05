90c26b503335fa6c8823ccfd3aa62dbd22173610
ststar8623 <ststar8623@gmail.com>
2017-05-05 16:06:30 -0700

    complete filter.test

--- test/filter.test.js
+++ test/filter.test.js
@@ -23,4 +23,23 @@ describe('filter()', () => {
     const abilityScores = _.filter(characterAttributes, (value) => !isNaN(value));
     expect(abilityScores).toEqual([4, 7, 10, 16, 5, 4]);
   });
+
+  // my own test
+  it('filters an object to only non-numeric values', () => {
+    const characterAttributes = {
+      name: 'Thokul Mongothsbeard',
+      honorific: 'Lord Thokul Mongothsbeard the Arcane',
+      race: 'human',
+      class: 'druid',
+      strength: 4,
+      constitution: 7,
+      dexterity: 10,
+      intelligence: 16,
+      wisdom: 5,
+      charisma: 4
+    };
+
+    const stringVal = _.filter(characterAttributes, (value) => isNaN(value));
+    expect(stringVal).toEqual(['Thokul Mongothsbeard', 'Lord Thokul Mongothsbeard the Arcane', 'human', 'druid']);
+  });
 });

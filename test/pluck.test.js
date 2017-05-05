5b64fbbc7d72372463ca4a5aace97ac014644525
ststar8623 <ststar8623@gmail.com>
2017-05-05 15:24:03 -0700

    complete pluck.test

--- test/pluck.test.js
+++ test/pluck.test.js
@@ -11,4 +11,16 @@ describe('pluck()', () => {
     expect(result).toEqual([12, 999, 14]);
   });
 
+  // my own test
+
+  it("returns an array of a given key's value", () => {
+    const people = [
+      { name: 'Harriet', age: 12},
+      { name: 'Lazarus', age: 999},
+      { name: 'Bethany', age: 14}
+    ];
+    const result = _.pluck(people, 'name');
+    expect(result).toEqual(['Harriet', 'Lazarus', 'Bethany']);
+  });
+
 });

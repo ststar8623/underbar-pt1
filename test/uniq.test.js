284f5b152a0935d25d805690ba4e9c0bc1c880e0
ststar8623 <ststar8623@gmail.com>
2017-05-05 16:12:20 -0700

    complete uniq.test

--- test/uniq.test.js
+++ test/uniq.test.js
@@ -5,4 +5,10 @@ describe('uniq()', () => {
     const nums = [2, 4, 5, 5, 7, 4, 10, 2];
     expect(_.uniq(nums)).toEqual([2, 4, 5, 7, 10]);
   });
+
+  // my own test
+  it('de-dups a list of strings and numbers', () => {
+    const strNum = ['steve', 'steven', 'steven', 'leung', 'steve', 3, 4, 5, 3];
+    expect(_.uniq(strNum)).toEqual(["steve", "steven", "leung", 3, 4, 5]);
+  });
 });
